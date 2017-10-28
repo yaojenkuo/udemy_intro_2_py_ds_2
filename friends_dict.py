1a8a219d4f9e1d41749be943ec1c3f6f5db198ef
yaojenkuo <tonykuoyj@gmail.com>
2017-10-28 21:39:18 +0800

    first commit

---
+++ friends_dict.py
@@ -0,0 +1,11 @@
+# 在這裡寫出您的答案，從您建立出的 dict 中選出 Chandler Bing
+friends_dict = {
+    "genre": "SitCom",
+    "seasons": 10,
+    "episodes": 236,
+    "characters": ["Rachel Green", "Monica Geller", "Phoebe Buffay", "Joey Tribbiani", "Chandler Bing", "Ross Geller"]
+}
+# 先選出 characters
+print(friends_dict["characters"])
+# 再選出 Chandler Bing
+print(friends_dict["characters"][4])
